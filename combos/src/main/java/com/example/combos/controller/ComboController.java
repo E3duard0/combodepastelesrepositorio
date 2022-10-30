02e42389851d0c9e76166505545f9bd9efce6c1b
E3duard0 <91648766+E3duard0@users.noreply.github.com>
2022-10-30 10:38:29 -0600

    servicio de combos de pasteles

---
+++ combos/src/main/java/com/example/combos/controller/ComboController.java
@@ -0,0 +1,25 @@
+package com.example.combos.controller;
+
+import com.example.combos.model.Combo;
+import com.example.combos.repo.ComboRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+
+@Controller
+public class ComboController {
+
+    @Autowired
+    private ComboRepository comboRepository;
+
+    @GetMapping
+    ModelAndView index(){
+        List<Combo> combo = comboRepository.findAll();
+
+        return new ModelAndView("index")
+                .addObject("comb", combo);
+    }
+}
